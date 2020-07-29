5971d23118c84ad42c74a5110a4544552f0d5bfc
Wojciech Pater <wpater@itersive.com>
2020-07-29 19:23:58 +0200

    use cloner service

--- src/main/java/com/examle/gitAnalyzer/service/GitRepoCloner.java
+++ src/main/java/com/examle/gitAnalyzer/service/GitRepoCloner.java
@@ -1,5 +1,6 @@
 package com.examle.gitAnalyzer.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.springframework.beans.factory.annotation.Value;
@@ -8,15 +9,20 @@ import org.springframework.stereotype.Service;
 import java.io.File;
 
 @Service
+@Slf4j
 public class GitRepoCloner {
     @Value("repo.path")
     private String path;
 
-    public void cloneRepository(String url) throws GitAPIException {
-        Git.cloneRepository()
-                .setURI(url)
-                .setDirectory(new File(path))
-                .call();
+    public void cloneRepository(String url) {
+        try {
+            Git.cloneRepository()
+                    .setURI(url)
+                    .setDirectory(new File(path))
+                    .call();
+        } catch (GitAPIException e) {
+            log.error("Failed to clone repository {} - {}", url, e.getMessage());
+        }
     }
 }
 
--- src/main/java/com/examle/gitAnalyzer/web/RepoController.java
+++ src/main/java/com/examle/gitAnalyzer/web/RepoController.java
@@ -1,6 +1,8 @@
 package com.examle.gitAnalyzer.web;
 
+import com.examle.gitAnalyzer.service.GitRepoCloner;
 import com.examle.gitAnalyzer.web.dto.CloneRepoDto;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,11 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/repo")
+@AllArgsConstructor
 public class RepoController {
+    private GitRepoCloner cloner;
 
     @PostMapping
     public ResponseEntity<?> cloneRepo(@RequestBody CloneRepoDto request) {
         System.out.println("Cloning repo: " + request.getRepoUrl() );
+        cloner.cloneRepository(request.getRepoUrl());
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
