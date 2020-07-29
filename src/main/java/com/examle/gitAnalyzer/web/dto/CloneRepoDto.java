64dde04bdb0a5e09d18c1c2abcc996a7113275f2
Svanar <kaamilwal@gmail.com>
2020-07-29 19:21:07 +0200

    Post for cloning repo

---
+++ src/main/java/com/examle/gitAnalyzer/web/dto/CloneRepoDto.java
@@ -0,0 +1,14 @@
+package com.examle.gitAnalyzer.web.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class CloneRepoDto {
+
+    private String repoUrl;
+
+}
